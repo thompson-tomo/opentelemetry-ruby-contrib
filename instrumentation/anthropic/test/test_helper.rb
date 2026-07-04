ea6c3b4da4a5e244282daaf5d27eaeb6108bdc4b
James Thompson <thompson.tomo@outlook.com>
2026-07-04 11:54:38 +1000

    anthropic tests

--- instrumentation/anthropic/test/test_helper.rb
+++ instrumentation/anthropic/test/test_helper.rb
@@ -4,6 +4,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+Dir["config/initializers/*.rb"].sort.each { |f| require_relative f }
+
 require 'bundler/setup'
 Bundler.require(:default, :development, :test)
 
